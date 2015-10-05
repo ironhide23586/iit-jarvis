5cc9020398326281ccc12f0d8a1fc31ff80342e4
Biswas Souham <sbiswas7@jarvis.cs.iit.edu>
2015-10-04 19:42:54 -0500

    rmoved crap

--- Assignment Stuff/Rough/hw2_ans_00/main.c
+++ Assignment Stuff/Rough/hw2_ans_00/main.c
@@ -6,7 +6,7 @@ int main()
     #pragma omp parallel num_threads(30)
     {
         int i;
-        printf("Hello world\n");
+        printf("Hello world LOLLLLLLL 2\n");
         #pragma omp for
         for (i = 1; i <=4; i++)
         {
