ab02292cbcecd4f2f3c6c443f0dfa7bf57398830
Biswas Souham <sbiswas7@jarvis.cs.iit.edu>
2015-10-04 19:55:26 -0500

    omp2 working on jarvis

--- Assignment Stuff/Rough/hw2_ans_00/main.c
+++ Assignment Stuff/Rough/hw2_ans_00/main.c
@@ -6,7 +6,7 @@ int main()
     #pragma omp parallel num_threads(30)
     {
         int i;
-        printf("Hello world LOLLLLLLL 2\n");
+        printf("Hello world LOLLLLLLL 2, OpenMP Version = %d\n", _OPENMP);
         #pragma omp for
         for (i = 1; i <=4; i++)
         {
