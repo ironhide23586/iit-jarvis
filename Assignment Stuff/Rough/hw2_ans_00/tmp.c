d06c34d1e9be498873f4dabd485432776591a23c
Biswas Souham <sbiswas7@jarvis.cs.iit.edu>
2015-10-06 03:28:15 -0500

    added code working on JARVIS

--- Assignment Stuff/Rough/hw2_ans_00/tmp.c
+++ Assignment Stuff/Rough/hw2_ans_00/tmp.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include <unistd.h>
 
 int main(int argc, char **argv)
@@ -7,10 +6,4 @@ int main(int argc, char **argv)
     int numCPU = sysconf( _SC_NPROCESSORS_ONLN );
     int k = ceil((float)3/4);
     printf("%d, %d\n", numCPU, k);
-=======
-
-int main(int argc, char **argv)
-{
-    numCPU = sysconf( _SC_NPROCESSORS_ONLN );
->>>>>>> be208cb98833e9d10d0536d6e27269d7ea367232
 }
