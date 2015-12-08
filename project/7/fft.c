af0584264218f56827435707db9fd5a5a9821db0
Biswas Souham <sbiswas7@jarvis.cs.iit.edu>
2015-12-07 22:20:49 -0600

    conv stub working (7)

--- project/7/fft.c
+++ project/7/fft.c
@@ -279,12 +279,26 @@ void compute()
 
 
 
-void func1(complex inp[N_lol][N_lol])
+void func1(complex *inp)
 {
     int i, j;
     for (i = 0; i < N_lol; i++)
         for(j = 0; j < N_lol; j++)
-            inp[i][j].r = i*N_lol + j;
+        {
+            inp[i * N_lol + j].r = i*N_lol + j;
+            inp[i * N_lol + j].i = 0;
+        }
+}
+
+void func2(complex *inp)
+{
+    int i, j;
+    for (i = 0; i < N_lol; i++)
+        for(j = 0; j < N_lol; j++)
+        {
+            inp[i * N_lol + j].r = (i*N_lol + j)/2;
+            inp[i * N_lol + j].i = 0;
+        }
 }
 
 void func(float inp[N_lol][N_lol])
@@ -293,19 +307,6 @@ void func(float inp[N_lol][N_lol])
     for (i = 0; i < N_lol; i++)
         for(j = 0; j < N_lol; j++)
             inp[i][j] = i*N_lol + j;
-}
-
-void print2DMatrixComp(complex mat[N_lol][N_lol])
-{
-    int i, j;
-    for (i = 0; i < N_lol; i++)
-    {
-        for(j = 0; j < N_lol; j++)
-        {
-            printf("%g\t", mat[i][j].r);
-        }
-        printf("\n");
-    }
 }
 
 void lolop(complex *arr, int n)
@@ -325,9 +326,8 @@ void lolop(complex *arr, int n)
     }
 }
 
-void processChunk(complex *myChunk, int blockSizeRows)
+void processFFTChunk(complex *myChunk, int blockSizeRows)
 {
-    //MPI_Recv(rData, N_lol * blockSizeRows, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     int i, j;
     for (i = 0; i < blockSizeRows; i++)
     {
@@ -345,6 +345,98 @@ void linearTranspose(complex *data)
         {
             C_SWAP(data[i * N_lol + j], data[j * N_lol + i]);
         }
+    }
+}
+
+void c_fft2d_parallel(complex *megaChunk, int nprocs, int blockSizeRows, int lastBlockSize, MPI_Datatype mpi_complex)
+{
+    int currBlockStart, limit, i, tmp;
+
+    for (tmp = 0; tmp < 2; tmp++)
+    {
+        for (i = 1; i < nprocs; i++)
+        {
+            currBlockStart = i * blockSizeRows;
+            if ((currBlockStart + blockSizeRows) > N_lol)
+                limit = lastBlockSize * N_lol;
+            else
+                limit = blockSizeRows * N_lol;
+
+            if (currBlockStart < N_lol)
+                MPI_Send((megaChunk + currBlockStart * N_lol), limit, mpi_complex, i, 0, MPI_COMM_WORLD);
+        }
+
+        for (i = 0; i < blockSizeRows; i++)
+        {
+            lolop(&megaChunk[i * N_lol], N_lol);
+        }
+
+        for (i = 1; i < nprocs - 1; i++)
+        {
+            MPI_Recv(&megaChunk[i * N_lol * blockSizeRows], blockSizeRows * N_lol, mpi_complex, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        MPI_Recv(&megaChunk[(nprocs - 1) * N_lol * blockSizeRows], lastBlockSize * N_lol, mpi_complex, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        linearTranspose(megaChunk);
+    }
+}
+
+void pointWiseMul_parallel(complex *megaChunk1, complex *megaChunk2, int nprocs, int blockSizeRows, int lastBlockSize, MPI_Datatype mpi_complex)
+{
+    int i, j;
+    for (i = 1; i < (nprocs - 1); i++)
+    {
+        MPI_Send((megaChunk1 + i * blockSizeRows * N_lol), blockSizeRows * N_lol, mpi_complex, i, 0, MPI_COMM_WORLD);
+        MPI_Send((megaChunk2 + i * blockSizeRows * N_lol), blockSizeRows * N_lol, mpi_complex, i, 1, MPI_COMM_WORLD);
+    }
+    MPI_Send((megaChunk1 + (nprocs - 1) * blockSizeRows * N_lol), lastBlockSize * N_lol, mpi_complex, i, 0, MPI_COMM_WORLD);
+    MPI_Send((megaChunk2 + (nprocs - 1) * blockSizeRows * N_lol), lastBlockSize * N_lol, mpi_complex, i, 1, MPI_COMM_WORLD);
+
+    for (i = 0; i < (blockSizeRows * N_lol); i++)
+    {
+        megaChunk1[i].r *= megaChunk2[i].r;
+        megaChunk1[i].i *= megaChunk2[i].i;
+    }
+
+    for (i = 1; i < (nprocs - 1); i++)
+    {
+        MPI_Recv((megaChunk1 + i * blockSizeRows * N_lol), blockSizeRows * N_lol, mpi_complex, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    MPI_Recv((megaChunk1 + (nprocs - 1) * blockSizeRows * N_lol), lastBlockSize * N_lol, mpi_complex, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+}
+
+
+void processMULChunk(complex *chunk0, complex *chunk1, int blockSizeRows)
+{
+    int i;
+    for (i = 0; i < blockSizeRows * N_lol; i++)
+    {
+        chunk0[i].r *= chunk1[i].r;
+        chunk0[i].i *= chunk1[i].i;
+    }
+}
+
+
+void subProcessMethod(int blockSizeRows, MPI_Datatype mpi_complex)
+{
+    int i;
+    complex *myChunk0 = (complex *)malloc(blockSizeRows * N_lol * sizeof(complex));
+    complex *myChunk1 = (complex *)malloc(blockSizeRows * N_lol * sizeof(complex));
+    for (i = 0; i < 4; i++)
+    {
+        MPI_Recv(myChunk0, N_lol * blockSizeRows, mpi_complex, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        processFFTChunk(myChunk0, blockSizeRows);
+        MPI_Send(myChunk0, blockSizeRows * N_lol, mpi_complex, 0, 0, MPI_COMM_WORLD);
+    }
+    MPI_Recv(myChunk0, N_lol * blockSizeRows, mpi_complex, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(myChunk1, N_lol * blockSizeRows, mpi_complex, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    processMULChunk(myChunk0, myChunk1, blockSizeRows);
+    MPI_Send(myChunk0, blockSizeRows * N_lol, mpi_complex, 0, 0, MPI_COMM_WORLD);
+    for (i = 0; i < 2; i++)
+    {
+        MPI_Recv(myChunk0, N_lol * blockSizeRows, mpi_complex, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        processFFTChunk(myChunk0, blockSizeRows);
+        MPI_Send(myChunk0, blockSizeRows * N_lol, mpi_complex, 0, 0, MPI_COMM_WORLD);
     }
 }
 
@@ -369,161 +461,65 @@ int main(int argc, int **argv)
     if (rank == 0)
     {
         printf("Last Block size = %d\n", lastBlockSize);
-        float lol1[N_lol][N_lol];
-        complex megaChunk[N_lol * N_lol];
-
-        func(lol1);
-
-        for (i = 0; i < N_lol; i++)
-        {
-            for (j = 0; j < N_lol; j++)
-            {
-                megaChunk[i * N_lol + j].r = lol1[i][j];
-                megaChunk[i * N_lol + j].i = 0;
-            }
-        }
-
-
-        for (i = 0; i < N_lol; i++)
-        {
-            for (j = 0; j < N_lol; j++)
-            {
-                printf("%e\t", megaChunk[i * N_lol + j].r);
-            }
-            printf("\n");
-        }
-
-        printf("BlockSize = %d\n", blockSizeRows);
-
-        int currBlockStart;
-        int limit;
-        for (i = 1; i < nprocs; i++)
-        {
-            currBlockStart = i * blockSizeRows;
-            if ((currBlockStart + blockSizeRows) > N_lol)
-                limit = lastBlockSize * N_lol;
-            else
-                limit = blockSizeRows * N_lol;
-
-            if (currBlockStart < N_lol)
-                MPI_Send((megaChunk + currBlockStart * N_lol), limit, mpi_complex, i, 0, MPI_COMM_WORLD);
-        }
-        printf("++++++++++++++++++++++\n");
-
-        for (i = 0; i < blockSizeRows; i++)
-        {
-            lolop(&megaChunk[i * N_lol], N_lol);
-        }
-
-
-        for (i = 1; i < nprocs - 1; i++)
-        {
-            MPI_Recv(&megaChunk[i * N_lol * blockSizeRows], blockSizeRows * N_lol, mpi_complex, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-
-        MPI_Recv(&megaChunk[(nprocs - 1) * N_lol * blockSizeRows], lastBlockSize * N_lol, mpi_complex, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        for (i = 0; i < N_lol; i++)
-        {
-            for (j = 0; j < N_lol; j++)
-            {
-                printf("%e\t", megaChunk[i * N_lol + j].r);
-            }
-            printf("\n");
-        }
-
-        linearTranspose(megaChunk);
+        complex megaChunk1[N_lol * N_lol], megaChunk2[N_lol * N_lol];
+        func1(megaChunk1);
+        func2(megaChunk2);
+        c_fft2d_parallel(megaChunk2, nprocs, blockSizeRows, lastBlockSize, mpi_complex);
+        c_fft2d_parallel(megaChunk1, nprocs, blockSizeRows, lastBlockSize, mpi_complex);
 
         printf("\n");
-
         for (i = 0; i < N_lol; i++)
         {
             for (j = 0; j < N_lol; j++)
             {
-                printf("%e\t", megaChunk[i * N_lol + j].r);
+                printf("%e\t", megaChunk1[i * N_lol + j].r);
             }
             printf("\n");
         }
 
-
-
-        for (i = 1; i < nprocs; i++)
-        {
-            currBlockStart = i * blockSizeRows;
-            if ((currBlockStart + blockSizeRows) > N_lol)
-                limit = lastBlockSize * N_lol;
-            else
-                limit = blockSizeRows * N_lol;
-
-            if (currBlockStart < N_lol)
-                MPI_Send((megaChunk + currBlockStart * N_lol), limit, mpi_complex, i, 0, MPI_COMM_WORLD);
-        }
-        printf("++++++++++++++++++++++\n");
-
-        for (i = 0; i < blockSizeRows; i++)
-        {
-            lolop(&megaChunk[i * N_lol], N_lol);
-        }
-
-
-        for (i = 1; i < nprocs - 1; i++)
-        {
-            MPI_Recv(&megaChunk[i * N_lol * blockSizeRows], blockSizeRows * N_lol, mpi_complex, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-
-        MPI_Recv(&megaChunk[(nprocs - 1) * N_lol * blockSizeRows], lastBlockSize * N_lol, mpi_complex, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
         printf("\n");
-
         for (i = 0; i < N_lol; i++)
         {
             for (j = 0; j < N_lol; j++)
             {
-                printf("%e\t", megaChunk[i * N_lol + j].r);
+                printf("%e\t", megaChunk2[i * N_lol + j].r);
             }
             printf("\n");
         }
 
-        linearTranspose(megaChunk);
+        pointWiseMul_parallel(megaChunk1, megaChunk2, nprocs, blockSizeRows, lastBlockSize, mpi_complex);
 
         printf("\n");
-
         for (i = 0; i < N_lol; i++)
         {
             for (j = 0; j < N_lol; j++)
             {
-                printf("%e\t", megaChunk[i * N_lol + j].r);
+                printf("%e\t", megaChunk1[i * N_lol + j].r);
+            }
+            printf("\n");
+        }
+
+        c_fft2d_parallel(megaChunk1, nprocs, blockSizeRows, lastBlockSize, mpi_complex);
+
+        printf("\n");
+        for (i = 0; i < N_lol; i++)
+        {
+            for (j = 0; j < N_lol; j++)
+            {
+                printf("%e\t", megaChunk1[i * N_lol + j].r);
             }
             printf("\n");
         }
     }
-
 
     if (rank > 0 & rank < (nprocs - 1))
     {
-        complex *myChunk = (complex *)malloc(blockSizeRows * N_lol * sizeof(complex));
-
-        MPI_Recv(myChunk, N_lol * blockSizeRows, mpi_complex, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        processChunk(myChunk, blockSizeRows);
-        MPI_Send(myChunk, blockSizeRows * N_lol, mpi_complex, 0, 0, MPI_COMM_WORLD);
-
-        MPI_Recv(myChunk, N_lol * blockSizeRows, mpi_complex, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        processChunk(myChunk, blockSizeRows);
-        MPI_Send(myChunk, blockSizeRows * N_lol, mpi_complex, 0, 0, MPI_COMM_WORLD);
+        subProcessMethod(blockSizeRows, mpi_complex);
     }
-
 
     if (rank == nprocs - 1)
     {
-        complex *myChunk = (complex *)malloc(lastBlockSize * N_lol * sizeof(complex));
-
-        MPI_Recv(myChunk, N_lol * lastBlockSize, mpi_complex, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        processChunk(myChunk, lastBlockSize);
-        MPI_Send(myChunk, lastBlockSize * N_lol, mpi_complex, 0, 0, MPI_COMM_WORLD);
-
-        MPI_Recv(myChunk, N_lol * lastBlockSize, mpi_complex, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        processChunk(myChunk, lastBlockSize);
-        MPI_Send(myChunk, lastBlockSize * N_lol, mpi_complex, 0, 0, MPI_COMM_WORLD);
+        subProcessMethod(lastBlockSize, mpi_complex);
     }
 
     MPI_Finalize();
